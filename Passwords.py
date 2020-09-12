41616f78e4cc4cedd3eafc0c51eba205fcd80a94
Ribbet400 <70328244+Ribbet400@users.noreply.github.com>
2020-09-12 16:22:52 +0100

    Original Commit
    Rickroll at 4s intervals 10 times (audio mixes and allows time for each page to load)

---
+++ Passwords.py
@@ -0,0 +1,10 @@
+# Fake password script
+"""
+Rickroll at 4s intervals 10 times
+"""
+import webbrowser
+import time
+webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', new=2)
+for i in range(10):
+    time.sleep(4)
+    webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', new=2)
